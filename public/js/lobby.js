dd82ddce333445aac4e88389ef81131b25219252
Travis Dunn <cmd@travisdunn.com>
2011-08-05 20:28:35 +0200

    prototype commit

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -4,14 +4,39 @@ var Lobby = function() {
 
   return {
     listen: function(userSessionId) {
+	  if (userSessionId === undefined) return;
+	
 	  var socket = io.connect('/lobby',	{'transports': TRANSPORTS});
+	
+	  function onDisconnect() {
+      }
+	
+	  function onAuthorized(data) {	
+        if (!data.authorized)	{
+        }
+      }
+	
+	  function onConnect() {
+	    socket.on('disconnect', onDisconnect);
+		socket.once('authorized', onAuthorized);
+		
+        socket.emit('authorizeUser', userSessionId);		
+	  }
+	
+	  function onConnectFailed() {	
+	  }
+	  function onReconnecting(){
+	  };
+	  function onReconnect(){
+	  };
+	  function onReconnectFailure(){
+	  };
 
-	  socket.on('connect', function () {
-        socket.emit('authorizeUser', userSessionId);
-	  });
-
-	  socket.on('connect_failed', function () {	
-	  });	
+	  socket.on('connect', onConnect);
+	  socket.on('connect_failed', onConnectFailed);	
+	  socket.on('reconnecting', onReconnecting);
+	  socket.on('reconnect', onReconnect);
+	  socket.on('reconnect_failure', onReconnectFailure);
     }
   };
 }();
