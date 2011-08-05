cbb68d67752dda983cd42e4c9432ab30dfcff8ae
Travis Dunn <cmd@travisdunn.com>
2011-08-05 18:44:09 +0200

    prototype commit

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,3 +1,17 @@
-(function() {
+var Lobby = function() {
+  var LOBBY_CHANNEL = 'lobby';
   var TRANSPORTS = ['websocket','flashsocket','xhr-polling','jsonp-polling','htmlfile'];
-})();
+
+  return {
+    listen: function(userSessionId) {
+	  var socket = io.connect('/lobby',	{'transports': TRANSPORTS});
+
+	  socket.on('connect', function () {
+        socket.emit('authorizeUser', userSessionId);
+	  });
+
+	  socket.on('connect_failed', function () {	
+	  });	
+    }
+  };
+}();
