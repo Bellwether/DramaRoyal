edbf4eac1aea1408db0e64fb9aad4e17a3c7d4ab
Travis Dunn <cmd@travisdunn.com>
2011-08-09 20:01:48 +0200

    prototype commit

--- controllers/app.js
+++ controllers/app.js
@@ -1,4 +1,10 @@
 module.exports = {
+  get_privacy: function(req, res){
+	res.render();
+  },
+  get_tos: function(req, res){
+	res.render();
+  },
   index: function(req, res){
 	res.render();
   }
