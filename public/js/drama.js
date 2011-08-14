d6b2082a2c5febaa5c33b8dbcdd9beb61bff20fe
Travis Dunn <cmd@travisdunn.com>
2011-08-14 20:07:50 +0200

    prototype commit

--- public/js/drama.js
+++ public/js/drama.js
@@ -1,4 +1,14 @@
 var Drama = function() {
+  function getGameIdFromUrl() {
+    var urlParts = window.location.toString().split('/'); 
+    var gameId = urlParts[ urlParts.length-1 ].split('?')[0].replace('#','');
+    return gameId;
+  }	
+  function isSecurePage() {
+	return location.protocol.indexOf('https') === 0;
+  };
+
+  var TRANSPORTS = ['websocket','flashsocket','xhr-polling','jsonp-polling','htmlfile'];
 }();
 
 // Expose Drama to the global object
