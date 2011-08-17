96a2133d5061e1d86c44580d7b66753c46dc61fd
Travis Dunn <cmd@travisdunn.com>
2011-08-17 23:12:13 +0200

    prototype commit

--- public/js/drama.js
+++ public/js/drama.js
@@ -1,4 +1,79 @@
 var outcomeUI = {
+  domPlayerPanel: function(){ return $('#game-players'); },
+  domOutcomePanel: function(){ return $('#turn-outcomes'); },
+  domOutcomeList: function(){ return outcomeUI.domOutcomePanel().children('ul').first(); },
+  domOutcomePages: function() { return $('#outcome-pages'); },
+  displayOutcomePage: function(outcome) {
+    var li = $('<li><p>'+outcome.description+'</p></li>');
+    li.appendTo(outcomeUI.domOutcomeList());
+  },
+  clearOutcomePages: function() {
+	outcomeUI.domOutcomePages().css({ 'marginLeft': 0}).data('page', 0).empty();
+  }.
+  applyDamage: function(outcome) {	
+    var targets = outcome.targets;
+	if (!targets) return;
+	
+    for(var idx = 0; idx < targets.length; idx++) {
+      var userId = targets[idx];
+      var esteem = parseInt(outcome.esteem);
+      if (esteem != 0) dramaUI.setPlayerEsteem(userId, esteem);
+    }	
+  },
+  applyTattles: function(outcome) {
+    var tattles = outcome.tattles;
+	if (!tattles) return;
+	
+    for(var idx = 0; idx < tattles.length; idx++) {
+      var playerItem = $('li#'+tattles[idx]);
+      var ownPlayerExists = playerItem.data('self');
+      if (ownPlayerExists) {
+        dramaUI.useTattle( tattles[idx] );
+        break;
+      }
+    }
+  },
+  applyLicks: function(outcome) {
+    var licks = outcome.licks;
+	if (!licks) return;
+	
+    for(var idx = 0; idx < licks.length; idx++) {
+	  var playerItem = $('li#'+licks[idx]);
+	  var ownPlayerExists = playerItem.data('self');
+      if (ownPlayerExists){
+	    dramaUI.useLick( outcome.licks[idx] );
+	    break;
+      }
+    }	
+  },
+  applyHumiliation: function(outcome) {
+    var humiliations = outcome.humiliations;
+	if (!humiliations) return;
+
+    for(var idx = 0; idx < humiliations.length; idx++) {
+	  var targetId = humiliations[idx];
+	  var playerElement = $('li#'+targetId);
+	
+	  playerElement.data('humilated', true);
+	  dramaUI.setPlayerStatus('humiliated', targetId);
+	  dramaUI.disableTargetPlayer(targerId);
+    }	
+  },
+  displayOutcome: function(data) {
+	outcomeUI.clearOutcomePages();
+	
+	for(var key in data) {
+	  var outcome = data[key];
+	
+      outcomeUI.displayOutcomePage(outcome);
+      outcomeUI.applyDamage(outcome);
+      outcomeUI.applyTattles(outcome);
+      outcomeUI.applyLicks(outcome);
+      outcomeUI.applyHumiliation(outcome);
+    }
+    dramaUI.hidePlayerPanel();
+    dramaUI.showOutcomePanel();
+  },
   init: function(list){	
     var isAnimating = false;
     var next = $('#' + list.data('next') );
@@ -29,9 +104,8 @@ var outcomeUI = {
 	
       if (pageDelta > 0) return false;
       if (pageDelta <= -numberOfPages) {
-	    var outcomePanel = $('#turn-outcomes');
-	    outcomePanel.hide();
-	    $('#game-players').show();
+	    dramaUI.hideOutcomePanel()
+	    dramaUI.showPlayerPanel();
 	    return false;
       }
 	
@@ -48,8 +122,11 @@ var dramaUI = {
   domConnectionStatus: function(){ return $('#chat-status'); },
   domChatButton: function(){ return $('#game-console-submit'); },
   domChatBox: function(){ return $('#game-message'); },	
+  domReadyButton: function(){ return $('#player-ready'); },
   domTattleButton: function(){ return $('#player-tattle'); },	
   domLickButton: function(){ return $('#player-lick'); },	
+  domOutcomePanel: function(){ return $('#outcome-pages'); },	
+
   stripNonNumeric: function(text) {
     return text.replace(/[^0-9]/g, ''); 
   },
@@ -82,6 +159,11 @@ var dramaUI = {
     esteem = Math.max( parseInt(esteem) + parseInt(delta), 0 );
     esteemBox.html('Self Esteem: '+esteem);
   },
+  disableTargetPlayer: function(targetId) {	
+    $('#player-'+targetId+'-tease').remove();
+    $('#player-'+targetId+'-scratch').remove();
+    $('#player-'+targetId+'-grab').remove();
+  },
   enableChat: function() {
     dramaUI.domChatButton().removeAttr("disabled").removeClass('disabled');
   },
@@ -94,6 +176,36 @@ var dramaUI = {
   enableGame: function() {
     $('.command').removeAttr("disabled").removeClass('disabled');
   }
+  blurGame: function() {
+    $('.command.selected').removeClass('selected');
+  },
+  removeGame: function() {
+    $('.command').remove();
+  },
+  removePlayer: function(userId) {
+	$('#player-list li').each(function(i) {
+      var itemExists = $(this).attr('id') === id.toString();
+	  if (itemExists) {
+        $(this).html('').attr('id', null);
+        return false;
+      }
+	});	
+  },
+  readyToWaiting: function() {	
+    dramaUI.domReadyButton().attr('disabled', 'disabled').unbind().text('waiting...');
+  },
+  hideOutcomePanel: function() {
+    outcomeUI.domOutcomePanel().hide();
+  },
+  showOutcomePanel: function() {
+    outcomeUI.domOutcomePanel().show();
+  },
+  showPlayerPanel: function() {
+    outcomeUI.domPlayerPanel().show();
+  },
+  hidePlayerPanel: function() {
+    outcomeUI.domPlayerPanel().hide();
+  },
   setConnectionStatus: function (status) {
     dramaUI.domConnectionStatus().text('Status: '+status);
   },
@@ -109,6 +221,16 @@ var dramaUI = {
       return false;
     };
     dramaUI.domChatButton().click(emitChatMessage);
+  },
+  initReadyControl: function(socket) {
+	dramaUI.domReadyButton().click(function (e) {
+      e.preventDefault();
+
+      socket.emit('game', {command: 'ready'}, function(){
+	    dramaUI.readyToWaiting();
+      });
+      return false;
+    };
   }
 }
 
@@ -122,10 +244,184 @@ var Drama = function() {
 	return location.protocol.indexOf('https') === 0;
   };
 
-  var TRANSPORTS = ['websocket','flashsocket','xhr-polling','jsonp-polling','htmlfile'];
+  var TRANSPORTS = ['websocket','flashsocket','xhr-polling','jsonp-polling','htmlfile'];	
+  var ONE_SECOND = 1000;
 
-  function onChat(data) { 
+  var gameAPI = {
+    gameInterval: null,
+    clearGameInterval: function() { 
+	  if (gameAPI.gameInterval) {
+		clearInterval(gameAPI.gameInterval);
+	  }
+	},
+	setGameInterval: function(seconds, time) {
+      gameAPI.gameInterval = setInterval(function() {
+        var seconds = time % 60;
+        if (seconds < 10) seconds = "0" + seconds; 
+        dramaUI.setClock(seconds);
+        time--;
+
+        if(time === 0) {
+	      setTimeout(function() { dramaUI.setClock("00") }, ONE_SECOND);
+          gameAPI.clearGameInterval();
+          return;
+        }
+      }, ONE_SECOND);		
+	},
+    domGameStatus: function(){ return $('#outcome-pages'); },
+    domPlayerStatus: function(playerId) { return $('#player-status-'+playerId); },
+    isGameInProgress: function() {
+      return gameAPI.domGameStatus().html().indexOf('Turn') >= 0;
+    },
+    isGameInCooldown: function() {
+      return gameAPI.domGameStatus().html().indexOf('next turn') >= 0;
+    },
+    isPlayerReady: function(userId) {
+	  return $('#player-status-'+userId).html().indexOf('ready') > 0);
+    },
+    setPlayerStatus: function(status, playerId) {
+	  gameAPI.domPlayerStatus(playerId).html(status ? '('+ status+')' : '');
+    },
+
+    startCountdown: function(seconds) {
+      var time = seconds;
+      dramaUI.setClock(time);	
+      time--;
+      gameAPI.setGameInterval(seconds, time);
+    },
+    turnStarted: function(data) {
+      gameAPI.startCountdown( 60 );
+
+      dramaUI.setTurn('Turn  ' + data.turn.cnt);
+      dramaUI.enableGame();
+      dramaUI.hideOutcomePanel()
+      dramaUI.showPlayerPanel();
+    };
+    turnEnded: function(data) {	
+      dramaUI.disableGame();
+      gameAPI.clearGameInterval();
+      outcomeUI.displayOutcome( data.turn.outcome );
+
+      dramaUI.setTurn('Waiting for next turn'); 
+      dramaUI.setClock('00');	
+      dramaUI.blurGame();
+    },
+    playerReady: function(data) {
+      var id = data.player._id;
+      var status = data.player.status;
+      gameAPI.setPlayerStatus('ready', id);
+    },
+    gameStarted: function(data){
+	  gameAPI.turnStarted( {"turn":{"cnt":1}} );
+    },
+    gameEnded: function(data) {
+	  dramaUI.removeGame();
+      gameAPI.clearGameInterval();
+      dramaUI.setTurn('Game Ended');
+      dramaUI.setClock();
+
+      var outcome = {'description': 'GAME OVER! '};
+      if (data.winners) {
+	    if (data.winners.length === 1) {
+	      outcome.description =	outcome.description + data.winners[0].name+" knew she was best as she watched other girls weep in defeat.";
+	    }
+	    else {
+	      for(var idx = 0; idx < data.winners.length; idx++) {
+		    outcome.description = outcome.description + data.winners[idx].name;
+		    if (idx < data.winners.length - 1) outcome.description = outcome.description + ' and ';
+	      }	
+	      outcome.description = outcome.description + " laughed in victory while the others cired.";
+	    }
+      } 
+      else {
+        outcome.description = "And oh what a mess! No girl was better than the others, each as worthless and ashamed as the next."	
+      }
+      outcomeUI.displayOutcomePage(outcome);
+    }
+    playerQuit: function(data) {
+      var id = data.player._id;
+
+      if ( gameAPI.isGameInProgress() ) {
+        dramaUI.setPlayerStatus('quit', id);
+      } 
+      else {
+	    dramaUI.removePlayer(id);
+      },
+    }
+    playerJoined = function(data){
+      var name = data.player._id;
+      var id = data.player._id;	
+
+      function makePlayerElement(li) { 
+        li.attr('id', id);
+	
+	    var chatBubble = $("<p id='player-chat-"+id+"' class='chat-bubble'></p>");
+	    var esteem = $("<div id='player-esteem-"+id+"' class='esteem'>Self Esteem: 10</div>");
+	    var img = $("<img class='paperdoll' src='/img/placeholder.png' />");
+	    var title = $("<span>"+id+" <span id='player-status-"+id+"'>(pending)</span></span>");
+
+	    var ctrlDiv = $("<div id='player-target-controls-"+id+"'></div");
+	    var tease = $("<button type='button' class='command' data-command='tease' disabled='disabled'>Tease</button>");
+	    var scratch = $("<button type='button' class='command' data-command='scratch' disabled='disabled'>Scratch</button>");
+	    var grab = $("<button type='button' class='command' data-command='grab' disabled='disabled'>Grab</button>");
+
+	    chatBubble.appendTo(li);
+	    esteem.appendTo(li);
+	    img.appendTo(li);
+	    title.appendTo(li);
+
+	    tease.appendTo(ctrlDiv);
+	    scratch.appendTo(ctrlDiv);
+	    grab.appendTo(ctrlDiv);
+	    ctrlDiv.appendTo(li);	
+      }
+
+      $('#player-list li').each(function(idx) {
+	    var itemEmptyOrExists = $(this).html() === '' || $(this).attr('id') === id.toString();
+        if (itemEmptyOrExists) {
+	      var li = $(this);
+          makePlayerElement(li);
+          return false;
+	    }
+      });	
+    };
+  }
+
+  function onChatEvent(data) { 
 	dramaUI.printMessage(data.message, data.userId);
+  };
+  function onPlayerEvent(data) {
+    switch (data.event) {
+	    case 'joined':
+	      gameAPI.playerJoined(data);
+	      break;			
+	    case 'quit':
+	      gameAPI.playerQuit(data);
+	      break;
+	    case 'ready':
+	      gameAPI.playerReady(data);
+	      break;		
+	  }
+  };
+  function onTurnEvent(data) {
+    switch (data.event) {
+      case 'started':	
+        gameAPI.turnStarted(data);
+        break;			
+      case 'ended':	
+        gameAPI.turnEnded(data);
+        break;	
+    }
+  };
+  function onGameEvent(data) {
+    switch (data.event) {
+      case 'started':
+        gameAPI.gameStarted(data);
+        break;
+      case 'ended':
+        gameAPI.gameEnded(data);
+        break;
+    }
   };
 
   return {
@@ -138,13 +434,38 @@ var Drama = function() {
 	
 	  function onAuthorized(data) {	
         if (data.authorized) {
-		  // socket.on('player', onGamePlayerEvent);
-		  // socket.on('game', onGameEvent);
-		  socket.on('chat', onChat);
+	      function registerSocketEvents(sckt) {
+		    sckt.on('player', onPlayerEvent);
+		    sckt.on('game', onGameEvent);
+		    sckt.on('turn', onTurnEvent);
+		    sckt.on('chat', onChatEvent);
+          }
+          registerSocketEvents(socket);
 
           dramaUI.setConnectionStatus('connected');	
           dramaUI.initChatControls(socket, data.userId);
-          dramaUI.enableChat(socket);		
+          dramaUI.enableChat(socket);	
+	
+	      function registerReadyButton(sckt) {
+            if (gameAPI.isPlayerReady(data.userId)) {
+	          dramaAPI.readyToWaiting();
+	        } else {
+              dramaUI.initReadyControl(sckt);
+            }
+	      }
+	      registerReadyButton(socket);
+
+          function intiateCountdownIfInProgress() {
+	        if ( gameAPI.isGameInProgress() && !gameAPI.isGameInCooldown() ) {
+              var seconds = $.stripNonNumeric( $('#turn-timer').html() );
+	          if (seconds.length > 0) gameAPI.startCountdown( parseInt(seconds) );
+            } else {
+              dramaAPI.disableGame();
+	        }
+	      }
+	      intiateCountdownIfInProgress();
+ 
+          outcomeUI.init(dramaUI.domOutcomePanel());
         } else {
 	      window.top.location = "/games";
         }
