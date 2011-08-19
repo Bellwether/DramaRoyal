9899dfcf3402ab2855e8aafad029667c4d340e26
Travis Dunn <cmd@travisdunn.com>
2011-08-19 21:03:20 +0200

    prototype commit

---
+++ models/score.js
@@ -0,0 +1,12 @@
+model = {}
+
+// br = ( (avg_num_votes * avg_rating) + (this_num_votes * this_rating) ) / (avg_num_votes + this_num_votes)
+// 
+// Legend:
+// 
+// avg_num_votes: The average number of votes of all items that have num_votes>0 
+// avg_rating: The average rating of each item (again, of those that have num_votes>0) 
+// this_num_votes: number of votes for this item 
+// this_rating: the rating of this item 
+// 
+// Note: avg_num_votes is used as the “magic” weight in this formula. The higher this value, the more votes it takes to influence the bayesian rating value.
